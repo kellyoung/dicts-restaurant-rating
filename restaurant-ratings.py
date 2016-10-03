f717963a8e0855447136f3f61732376c2d412277
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 16:30:44 -0700

    Allowing user to input new restaurant

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -6,6 +6,11 @@ def get_restaurant_ratings(rating_file):
             restaurant, rating = restaurant_line.rstrip().split(':')
             restaurants_and_ratings[restaurant] = rating
 
+    user_restaurant = raw_input("Please provide a restaurant name: ")
+    user_rating = raw_input("Please provide the restaurant's rating (1-5): ")
+
+    restaurants_and_ratings[user_restaurant] = int(user_rating)
+
     sorted_restaurants = sorted(restaurants_and_ratings.items())
 
     for restaurant, rating in sorted_restaurants:
