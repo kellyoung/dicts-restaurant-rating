3a69b641ead82d96711baa39ccd56ddb1b3cb606
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 17:41:30 -0700

    Allow user to rate a random restaurant

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,3 +1,5 @@
+import random
+
 def get_restaurant_ratings(rating_file):
     restaurants_and_ratings = {}
     
@@ -8,8 +10,9 @@ def get_restaurant_ratings(rating_file):
 
     while True:
         print "\n"
-        print ("Would you like to see restaurant ratings (1) or rate"
-               " a restaurant(2) or quit(3)?")
+        print ("Would you like to see restaurant ratings (1), or rate"
+               " a new restaurant(2), or update a random restaurant's rating(3),"
+               " or quit(4)?")
         user_choice = raw_input("Make your choice: ")
 
         if user_choice == '1':
@@ -23,8 +26,18 @@ def get_restaurant_ratings(rating_file):
             user_rating = raw_input("Please provide the restaurant's rating (1-5): ")
 
             restaurants_and_ratings[user_restaurant] = int(user_rating)
+
         elif user_choice == '3':
+            random_rest = random.choice(restaurants_and_ratings.keys())
+            print ("What is your new rating for {}. Current rating."
+                  " is {}".format(random_rest, restaurants_and_ratings[random_rest]))
+            user_new_rating = raw_input("Enter rating between 1 and 5: ")
+
+            restaurants_and_ratings[random_rest] = int(user_new_rating)
+        
+        elif user_choice == '4':
             break
+        
         else:
             print "Try again"
 
